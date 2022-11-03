bf2356c6e1190d1cbfaee017e1500ef454a2c8de
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-11-02 21:19:23 -0300

    icones linkedin + github

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { Bg } from "../../components/Bg/Bg";
 import { Logo } from "../../components/Logo/Logo";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { GithubLogo, LinkedinLogo } from "phosphor-react";
 import "./styles.css";
 
 export function Home() {
@@ -10,8 +11,21 @@ export function Home() {
       <Navbar />
       <div className="logo-star-container">
         <Logo />
-        <p className="copy-style">&copy; 2022 Copyright: Bruno Nascimento</p>
       </div>
+      <footer className="icon-container">
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/brunonascimento1996/"
+            target={"_blank"}
+          >
+            <LinkedinLogo size={42} weight="fill" />
+          </a>
+          <a href="https://github.com/brunonascimentoo" target={"_blank"}>
+            <GithubLogo size={42} weight="fill" />
+          </a>
+        </div>
+        <p className="copy-style">&copy; 2022 Copyright: Bruno Nascimento</p>
+      </footer>
     </>
   );
 }
